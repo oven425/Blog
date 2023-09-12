a3e2f9949d75f521eacded48a36a986231ac96c8
oven425 <oven425@yahoo.com.tw>
2023-09-13 00:30:13 +0800

    update 1. study.....

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { StaticQueryDocument, graphql } from 'gatsby'
+import { StaticQueryDocument, graphql, PageProps } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 type DataType = {
@@ -10,21 +10,39 @@ type DataType = {
     }
   }
 //https://www.gatsbyjs.com/blog/getting-started-with-typescript-in-gatsby
-const BlogPage = ({ data }) => {
+
+const BlogPage = ({ data }: PageProps<DataType>) => {
+  //console.log(data.allFile)
   return (
     <Layout pageTitle="My Blog Posts">
       <ul>
-      {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
-        ))
-      }
+        {
+          data.allFile.nodes.map(x=>(
+            <li>{x.name}</li>
+          ))
+        }
+      
       </ul>
     </Layout>
   )
 }
+
+// const BlogPage = ({ data: { allFile } }: PageProps<DataType>) => {
+//   return (
+//     <Layout pageTitle="My Blog Posts">
+//       <ul>
+//       {
+      
+//         data.allFile.nodes.map(node => (
+//           <li key={node.name}>
+//             {node.name}
+//           </li>
+//         ))
+//       }
+//       </ul>
+//     </Layout>
+//   )
+// }
 
 export const query = graphql`
   query {
