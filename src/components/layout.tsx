9fa930818e4da5badad161a5a3551e80898f563f
oven425 <oven425@yahoo.com.tw>
2023-10-02 12:03:33 +0800

    update

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,52 +1,71 @@
-import * as React from 'react'
-import { Link, useStaticQuery, graphql } from 'gatsby'
-import {
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-  siteTitle
-} from './layout.module.css'
-import { ReactNode } from '@mdx-js/react/lib';
+// import * as React from 'react'
+// import { Link, useStaticQuery, graphql } from 'gatsby'
+// import {
+//   container,
+//   heading,
+//   navLinks,
+//   navLinkItem,
+//   navLinkText,
+//   siteTitle
+// } from './layout.module.css'
+// import { ReactNode } from '@mdx-js/react/lib';
 
-type AppProps = {
-    pageTitle: string;
-    children : ReactNode;
-  };
-const Layout = ({ pageTitle, children }: AppProps) => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-  return (
-    <div className={container}>
-      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
-          <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
-          <li className={navLinkItem}>
-            <Link to="/blog" className={navLinkText}>
-              Blog
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-      </main>
+import React, { ReactNode } from "react";
+
+// type AppProps = {
+//     pageTitle: string;
+//     children : ReactNode;
+//   };
+// const Layout = ({ pageTitle, children }: AppProps) => {
+//   const data = useStaticQuery(graphql`
+//     query {
+//       site {
+//         siteMetadata {
+//           title
+//         }
+//       }
+//     }
+//   `)
+//   return (
+//     <div className={container}>
+//       <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+//       <nav>
+//         <ul className={navLinks}>
+//           <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+//           <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+//           <li className={navLinkItem}>
+//             <Link to="/blog" className={navLinkText}>
+//               Blog
+//             </Link>
+//           </li>
+//         </ul>
+//       </nav>
+//       <main>
+//         <h1 className={heading}>{pageTitle}</h1>
+//         {children}
+//       </main>
+//     </div>
+//   )
+// }
+
+// export default Layout
+
+// //https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forms_and_events
+// //https://www.gatsbyjs.com/docs/tutorial/getting-started/part-3/
+type LayoutProps = {
+  title?:string,
+  children?:ReactNode
+}
+const Layout = (props:LayoutProps)=>{
+  return(
+    <div className="grid grid-rows-2 h-screen w-screen bg-red-300">
+      <div>Header</div>
+        <div className="h-full">
+          <div>Sider</div>
+          <div>Content</div>
+        </div>
     </div>
   )
 }
 
-export default Layout
-
-//https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forms_and_events
-//https://www.gatsbyjs.com/docs/tutorial/getting-started/part-3/
+export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -192,90 +192,142 @@
 
 // export const Head: HeadFC = () => <title>Home Page</title>
 import * as React from 'react'
-import { Link } from 'gatsby'
-import Layout from '../components/layout';
+import { Link, PageProps, graphql } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import Seo from '../components/seo'
 import ChildWindow, { ChildWindow_Ref } from '../components/ChildWindow';
 import { useRef } from 'react';
+import Layout from '../components/layout';
 
-const IndexPage = () => {
-  const cw = useRef<ChildWindow_Ref>(null)
-  const open = () => {
 
-  }
+// type allMdxType = {
+//   allMdx: {
+//     nodes: {
+//       frontmatter: {
+//         date: string,
+//         title: string
+//       },
+//       id: string,
+//       excerpt: string
+//     }[]
+//   }
+// }
+
+// const IndexPage = ({ data }: PageProps<allMdxType>) => {
+//   return (
+//     // <Layout pageTitle="Home Page">
+//     //   <p>I'm making this by following the Gatsby Tutorial.</p>
+//     //   <StaticImage
+//     //     alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+//     //     src="../images/clifford.jpg"
+//     //   />
+//     // </Layout>
+
+//     <div className="flex flex-col h-screen">
+//       <header className=" bg-gray-950 h-16 shrink-0">
+//         <div className="flex items-center justify-between h-full ">
+//           <div className="text-white text-3xl font-bold p-1">
+//             Developer notes
+//           </div>
+//           <div className=" bg-slate-50 p-1">
+//             <a href='https://github.com/oven425'>
+//               <svg height="36" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="36" data-view-component="true" className="octicon octicon-mark-github v-align-middle color-fg-default">
+//                 <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+//               </svg>
+//             </a>
+//           </div>
+//         </div>
+//       </header>
+//       <div className="flex flex-row h-[calc(100vh-5rem)] bg-white">
+//         <aside className="w-1/6 overflow-y-auto">
+//           <ul>
+//           <Link to='/'>Blog</Link>
+//             <Link to='/about'>About</Link>
+//             </ul>
+            
+//         </aside>
+//         <main className="w-5/6 overflow-y-auto">
+//         <ul>
+//         {
+//           data.allMdx.nodes.map((node) => (
+//             <article key={node.id}>
+//               <h2>{node.frontmatter.title}</h2>
+//               <p>Posted: {node.frontmatter.date}</p>
+//               <p>{node.excerpt}</p>
+//             </article>
+//           ))
+//         }
+
+//       </ul>
+//           {/* main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br /> */}
+//         </main>
+//       </div>
+//       {/* <ChildWindow ref={cw} title='TestAA'>
+//         <div className="w-full max-w-xs bg-white">
+//           <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+//             <div className="mb-4">
+//               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+//                 Username
+//               </label>
+//               <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+//             </div>
+//             <div className="mb-6">
+//               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+//                 Password
+//               </label>
+//               <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+//               <p className="text-red-500 text-xs italic">Please choose a password.</p>
+//             </div>
+//             <div className="flex items-center justify-between">
+//               <button onClick={x => cw.current?.close()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+//                 Sign In
+//               </button>
+//               <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+//                 Forgot Password?
+//               </a>
+//             </div>
+//           </form>
+//           <p className="text-center text-gray-500 text-xs">
+//             &copy;2020 Acme Corp. All rights reserved.
+//           </p>
+//         </div>
+//       </ChildWindow> */}
+//     </div>
+
+//   )
+// };
+
+const IndexPage = ()=>{
+  const time = useRef(null);
+  React.useEffect(()=>{
+    
+  },[])
   return (
-    // <Layout pageTitle="Home Page">
-    //   <p>I'm making this by following the Gatsby Tutorial.</p>
-    //   <StaticImage
-    //     alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-    //     src="../images/clifford.jpg"
-    //   />
-    // </Layout>
-
-    <div className="flex flex-col h-screen">
-      <header className=" bg-gray-950 h-20 shrink-0">
-        <div className="flex items-center justify-between h-full ">
-          <div className="text-white text-3xl font-bold p-1">
-            Developer notes
-          </div>
-          <div className=" bg-slate-50 p-1">
-            <a href='https://github.com/oven425'>
-              <svg height="36" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="36" data-view-component="true" className="octicon octicon-mark-github v-align-middle color-fg-default">
-                <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-              </svg>
-            </a>
-          </div>
-        </div>
-      </header>
-      <div className="flex flex-row h-[calc(100vh-5rem)]">
-        <aside className="bg-red-500 w-1/6 overflow-y-auto">
-          <div>
-            <button onClick={() => cw.current?.open()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Open Childwindow
-            </button>
-            <Link to='aa'>Blog</Link>
-            <Link to='aa'>About</Link>
-          </div>
-
-        </aside>
-        <main className="bg-blue-500 w-5/6 overflow-y-auto">
-          main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />main content<br />
-        </main>
-      </div>
-      <ChildWindow ref={cw} title='TestAA'>
-        <div className="w-full max-w-xs bg-white">
-          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                Username
-              </label>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
-            </div>
-            <div className="mb-6">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                Password
-              </label>
-              <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-              <p className="text-red-500 text-xs italic">Please choose a password.</p>
-            </div>
-            <div className="flex items-center justify-between">
-              <button onClick={x => cw.current?.close()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                Sign In
-              </button>
-              <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-                Forgot Password?
-              </a>
-            </div>
-          </form>
-          <p className="text-center text-gray-500 text-xs">
-            &copy;2020 Acme Corp. All rights reserved.
-          </p>
-        </div>
-      </ChildWindow>
+    <div ref={time}>
+        <p ></p>
     </div>
+)
 
-  )
-};
+  // return(
+  //   <Layout>
+      
+  //   </Layout>
+  // )
+}
+
+export const query = graphql`
+  query {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
+      }
+    }
+  }
+`
 export const Head = () => <Seo title="Home Page" />
 export default IndexPage;
